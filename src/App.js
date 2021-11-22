d8fcd459a3824da04de9b2a309c1376b8ddb2f65
Mark Bennett <mark@burmis.ca>
2021-11-22 12:30:02 -0700

    Connect the wallet.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  // Actions
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
@@ -29,6 +33,11 @@ const App = () => {
           'Connected with Public Key:',
           response.publicKey.toString()
         );
+
+        /*
+         * Set the user's publicKey in state to be used later!
+         */
+        setWalletAddress(response.publicKey.toString());
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
       }
@@ -41,7 +50,15 @@ const App = () => {
    * Let's define this method so our code doesn't break.
    * We will write the logic for this next!
    */
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const { solana } = window;
+  
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   /*
    * We want to render this UI when the user hasn't connected
@@ -56,6 +73,7 @@ const App = () => {
     </button>
   );
 
+  // useEffects
   /*
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
@@ -70,14 +88,15 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      {/* This was solely added for some styling fanciness */}
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">📖 Emoji Cookbook</p>
           <p className="sub-text">
             Share recipes for your favourite emoji mixups ✨
           </p>
           {/* Render your connect to wallet button right here */}
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
           <p className="sub-text">
             Inspired by <a href="https://tikolu.net/emojimix/">emojimix</a>
           </p>
